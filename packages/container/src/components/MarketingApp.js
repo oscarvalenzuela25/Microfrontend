3ae147320408491f7983bc5dd38ec2969d2f9c05
Oscar Valenzuela <oscar@xepelin.com>
2024-09-09 23:21:09 -0300

    commit de testeo

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
+  // Ref para referenciar el div donde se montara el componente
   const ref = useRef(null);
 
   useEffect(() => {
