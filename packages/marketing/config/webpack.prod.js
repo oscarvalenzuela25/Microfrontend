e57a3b41c8b08b6886e9d472f6a10c8241391de2
Oscar Valenzuela <oscar@xepelin.com>
2024-09-13 18:10:54 -0300

    changes in webpack.prod

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/marketing/latest/',
+    publicPath: '/marketing/latest/', // Ruta del bucket de s3 a exponer el archivo remoteEntry.js
   },
   plugins: [
     new ModuleFederationPlugin({
