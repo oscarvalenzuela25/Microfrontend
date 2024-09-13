1b9c865166a3c4d1f204232c37fc4f57c84dd4b3
Oscar Valenzuela <oscar@xepelin.com>
2024-09-13 18:18:50 -0300

    console.log

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 const App = () => {
+  const domain = process?.env?.PRODUCTION_DOMAIN || '';
+  console.log(domain);
   return <MarketingApp />;
 };
 
