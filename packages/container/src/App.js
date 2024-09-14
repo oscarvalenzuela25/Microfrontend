405f1b0d9445264d88d6f9f11fdefe5cf3c0180a
Oscar Valenzuela <oscar@xepelin.com>
2024-09-14 14:37:57 -0300

    Add Header component to container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 const App = () => {
-  return <MarketingApp />;
+  return (
+    <Router>
+      <Switch>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
