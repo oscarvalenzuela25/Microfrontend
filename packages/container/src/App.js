06eeb50b2ace6c9a569b0e6d957c7c4fce0860ad
Oscar Valenzuela <oscar@xepelin.com>
2024-09-14 16:24:16 -0300

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+// Cuando se tienen las mismas librerias de framework de css, cuando se buildea el proyecto para prod
+// Se generan clases con nombres iguales, para evitar esto se puede usar el metodo createGenerateClassName
+// Entonces asi los hijos y padres que usen la misma libreria, no van a chocar con los nombres autogenerados
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
       </Switch>
     </Router>
   );
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
 
+// Cuando se tienen las mismas librerias de framework de css, cuando se buildea el proyecto para prod
+// Se generan clases con nombres iguales, para evitar esto se puede usar el metodo createGenerateClassName
+// Entonces asi los hijos y padres que usen la misma libreria, no van a chocar con los nombres autogenerados
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <Router>
         <Switch>
           <Route path="/pricing" component={Pricing} />
